146b2f8c73551342a42a2ed044df8d3d437d50c8
Niklas Neronin <niklas.neronin@linux.intel.com>
2024-03-26 13:13:33 +0200

    usb: xhci: remove infinite loop prevention
    If a buggy HW reports some unpredicted event (for example, an overrun event
    following a MSE event while the EP ring is actually not empty), the driver
    will never find the TD, and it will loop until the TD list is empty.
    
    Before commits [1][2], the spin lock was released when giving back a URB in
    the do-while loop. This could cause more TD to be added to TD list, causing
    an infinite loop.
    
    Because of commits [1][2] the spin lock is not released any more, thus the
    infinite loop prevention is unnecessary and is removed.
    
    [1], commit 0c03d89d0c71 ("xhci: Giveback urb in finish_td directly")
    [2], commit 36dc01657b49 ("usb: host: xhci: Support running urb giveback in
    			   tasklet context")
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -2669,7 +2669,6 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 	int status;
 	struct xhci_ep_ctx *ep_ctx;
 	u32 trb_comp_code;
-	int td_num = 0;
 
 	slot_id = TRB_TO_SLOT_ID(le32_to_cpu(event->flags));
 	ep_index = TRB_TO_EP_ID(le32_to_cpu(event->flags)) - 1;
@@ -2697,10 +2696,6 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 			goto err_out;
 		return 0;
 	}
-
-	/* Count current td numbers if ep->skip is set */
-	if (ep->skip)
-		td_num += list_count_nodes(&ep_ring->td_list);
 
 	/* Get the error status for the completion code. */
 	if (xhci_is_vendor_info_code(xhci, trb_comp_code)) {
@@ -2742,18 +2737,8 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 			return 0;
 		}
 
-		/* We've skipped all the TDs on the ep ring when ep->skip set */
-		if (ep->skip && td_num == 0) {
-			ep->skip = false;
-			xhci_dbg(xhci, "All tds on the ep_ring skipped. Clear skip flag for slot %u ep %u.\n",
-				 slot_id, ep_index);
-			return 0;
-		}
-
 		td = list_first_entry(&ep_ring->td_list, struct xhci_td,
 				      td_list);
-		if (ep->skip)
-			td_num--;
 
 		/* Is this a TRB in the currently executing TD? */
 		ep_seg = trb_in_td(ep_ring, td, ep_trb_dma);
