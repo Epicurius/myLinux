0b787e1c1e56c3f5bcd5303ac2264e0ecd67eee5
Niklas Neronin <niklas.neronin@linux.intel.com>
2024-05-07 11:37:29 +0300

    usb: xhci: prevent duplicate events from clearing the ring
    In some cases a device might generate duplicate events pointing to the
    same TRB, after the first event is handled the TRB is dequeued, causing
    the second event to print a misleading warning. To prevent this, check
    whether a TRB is in queue before searching for its TD.
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -327,6 +327,11 @@ static struct xhci_segment *trb_in_td(struct xhci_td *td, dma_addr_t dma)
 	return dma_in_range(td->start_seg, td->start_trb, td->end_seg, td->end_trb, dma);
 }
 
+static struct xhci_segment *trb_in_queue(struct xhci_ring *ring, dma_addr_t dma)
+{
+	return dma_in_range(ring->deq_seg, ring->dequeue, ring->enq_seg, ring->enqueue, dma);
+}
+
 /*
  * Return number of free normal TRBs from enqueue to dequeue pointer on ring.
  * Not counting an assumed link TRB at end of each TRBS_PER_SEGMENT sized segment.
@@ -2750,6 +2755,16 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 
 		ep->skip = false;
 		goto check_endpoint_halted;
+	}
+
+	/*
+	 * The device might generate duplicate events, in this case the DMA address may point to a
+	 * TRB which is outside of the ring queueu.
+	 */
+	ep_seg = trb_in_queue(ep_ring, ep_trb_dma);
+	if (!ep_seg) {
+		xhci_dbg(xhci, "Event TRB for slot %u ep %u not in queue\n", slot_id, ep_index);
+		return 0;
 	}
 
 	do {
