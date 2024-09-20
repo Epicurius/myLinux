6b232d1cef729f8542e07aaa2937f327a3d01441
Niklas Neronin <niklas.neronin@linux.intel.com>
2024-09-20 12:07:56 +0300

    usb: xhci: rework xhci_add_interrupter()
    Refactor xhci_add_interrupter() to focus solely on initializing the
    interrupter, allowing it to be decoupled from xhci_alloc_interrupter().
    
     * Move the interrupt array pointer setting out of this function.
    
     * Remove the first error check, as it's incorrect. The 'max_interrupters'
       argument represents the maximum number of interrupters IF available.
       For PCI, the number of interrupters ranges from 1 to max_interrupters,
       while for other cases, it is typically 1. This issue will be addressed
       in a separate patch set due to its complexity. For now, remove this
       error check since the xhci driver currently supports only interrupter 0.
    
     * Change the function to return void, as the xhci driver only supports one
       interrupter. Error checks are unnecessary and handling error cases adds
       unnecessary complexity.
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci-mem.c
+++ drivers/usb/host/xhci-mem.c
@@ -2288,14 +2288,12 @@ xhci_alloc_interrupter(struct xhci_hcd *xhci, unsigned int segs, gfp_t flags)
 	return ir;
 }
 
-static void
-xhci_add_interrupter(struct xhci_hcd *xhci, struct xhci_interrupter *ir,
-		     unsigned int intr_num)
+static void xhci_init_interrupter(struct xhci_hcd *xhci, unsigned int intr_num)
 {
+	struct xhci_interrupter *ir = xhci->interrupters[intr_num];
 	u64 erst_base;
 	u32 erst_size;
 
-	xhci->interrupters[intr_num] = ir;
 	ir->intr_num = intr_num;
 	ir->ir_set = &xhci->run_regs->ir_set[intr_num];
 
@@ -2338,8 +2336,9 @@ xhci_create_secondary_interrupter(struct usb_hcd *hcd, unsigned int segs,
 	/* Find available secondary interrupter, interrupter 0 is reserved for primary */
 	for (i = 1; i < xhci->max_interrupters; i++) {
 		if (xhci->interrupters[i] == NULL) {
-			xhci_add_interrupter(xhci, ir, i);
-                        err = 0;
+			xhci->interrupters[i] = ir;
+			xhci_init_interrupter(xhci, i);
+			err = 0;
 			break;
 		}
 	}
@@ -2413,7 +2412,6 @@ static void xhci_set_dev_notifications(struct xhci_hcd *xhci)
 
 int xhci_mem_init(struct xhci_hcd *xhci, gfp_t flags)
 {
-	struct xhci_interrupter *ir;
 	struct device	*dev = xhci_to_hcd(xhci)->self.sysdev;
 	dma_addr_t	dma;
 	unsigned int	val;
@@ -2511,13 +2509,12 @@ int xhci_mem_init(struct xhci_hcd *xhci, gfp_t flags)
 	xhci->interrupters = kcalloc_node(xhci->max_interrupters, sizeof(*xhci->interrupters),
 					  flags, dev_to_node(dev));
 
-	ir = xhci_alloc_interrupter(xhci, 0, flags);
-	if (!ir)
+	xhci->interrupters[0] = xhci_alloc_interrupter(xhci, 0, flags);
+	if (!xhci->interrupters[0])
 		goto fail;
 
-	xhci_add_interrupter(xhci, ir, 0);
-
-	ir->isoc_bei_interval = AVOID_BEI_INTERVAL_MAX;
+        xhci->interrupters[0]->isoc_bei_interval = AVOID_BEI_INTERVAL_MAX;
+	xhci_init_interrupter(xhci, 0);
 
 	for (i = 0; i < MAX_HC_SLOTS; i++)
 		xhci->devs[i] = NULL;
