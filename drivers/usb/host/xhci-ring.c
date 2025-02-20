20df08b663881d19d28356eab5049d692ea3cff9
Niklas Neronin <niklas.neronin@linux.intel.com>
2025-02-20 14:32:23 +0200

    usb: xhci: modify trb_in_td() to be more modular
    Lay the ground work for future handle_tx_event() rework, which will
    require a function which checks if a DMA address is in queueu.
    
    To its core, trb_in_td() checks if a TRB falls within the specified start
    and end TRB/segment range, a common requirement. For instance, a ring has
    pointers to the queues first and last TRB/segment, which means that with
    slight modifications and renaming trb_in_td() could work for other
    structures not only TDs.
    
    Modify trb_in_td() to accept pointer to start and end TRB/segment, and
    introduce a new function that takes a 'xhci_td' struct pointer, forwarding
    its elements to dma_in_range(), previously trb_in_td().
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -278,18 +278,22 @@ static void inc_enq(struct xhci_hcd *xhci, struct xhci_ring *ring,
 }
 
 /*
- * If the suspect DMA address is a TRB in this TD, this function returns that
- * TRB's segment. Otherwise it returns 0.
+ * Check if the DMA address of a TRB falls within the specified range.
+ * The range is defined by 'start_trb' in 'start_seg' and 'end_trb' in 'end_seg'.
+ * If the TRB's DMA address is within this range, return the segment containing the TRB.
+ * Otherwise, return 'NULL'.
  */
-static struct xhci_segment *trb_in_td(struct xhci_td *td, dma_addr_t suspect_dma)
+static struct xhci_segment *dma_in_range(struct xhci_segment *start_seg, union xhci_trb *start_trb,
+					 struct xhci_segment *end_seg, union xhci_trb *end_trb,
+					 dma_addr_t dma)
 {
 	dma_addr_t start_dma;
 	dma_addr_t end_seg_dma;
 	dma_addr_t end_trb_dma;
 	struct xhci_segment *cur_seg;
 
-	start_dma = xhci_trb_virt_to_dma(td->start_seg, td->start_trb);
-	cur_seg = td->start_seg;
+	start_dma = xhci_trb_virt_to_dma(start_seg, start_trb);
+	cur_seg = start_seg;
 
 	do {
 		if (start_dma == 0)
@@ -298,34 +302,39 @@ static struct xhci_segment *trb_in_td(struct xhci_td *td, dma_addr_t suspect_dma
 		end_seg_dma = xhci_trb_virt_to_dma(cur_seg,
 				&cur_seg->trbs[TRBS_PER_SEGMENT - 1]);
 		/* If the end TRB isn't in this segment, this is set to 0 */
-		end_trb_dma = xhci_trb_virt_to_dma(cur_seg, td->end_trb);
+		end_trb_dma = xhci_trb_virt_to_dma(cur_seg, end_trb);
 
 		if (end_trb_dma > 0) {
 			/* The end TRB is in this segment, so suspect should be here */
 			if (start_dma <= end_trb_dma) {
-				if (suspect_dma >= start_dma && suspect_dma <= end_trb_dma)
+				if (dma >= start_dma && dma <= end_trb_dma)
 					return cur_seg;
 			} else {
 				/* Case for one segment with
 				 * a TD wrapped around to the top
 				 */
-				if ((suspect_dma >= start_dma &&
-							suspect_dma <= end_seg_dma) ||
-						(suspect_dma >= cur_seg->dma &&
-						 suspect_dma <= end_trb_dma))
+				if ((dma >= start_dma &&
+							dma <= end_seg_dma) ||
+						(dma >= cur_seg->dma &&
+						 dma <= end_trb_dma))
 					return cur_seg;
 			}
 			return NULL;
 		}
 		/* Might still be somewhere in this segment */
-		if (suspect_dma >= start_dma && suspect_dma <= end_seg_dma)
+		if (dma >= start_dma && dma <= end_seg_dma)
 			return cur_seg;
 
 		cur_seg = cur_seg->next;
 		start_dma = xhci_trb_virt_to_dma(cur_seg, &cur_seg->trbs[0]);
-	} while (cur_seg != td->start_seg);
+	} while (cur_seg != start_seg);
 
 	return NULL;
+}
+
+static struct xhci_segment *trb_in_td(struct xhci_td *td, dma_addr_t dma)
+{
+	return dma_in_range(td->start_seg, td->start_trb, td->end_seg, td->end_trb, dma);
 }
 
 /*
