298cc70d5ab92c47f61eff20ee5110a040078e1a
Niklas Neronin <niklas.neronin@linux.intel.com>
2025-03-20 13:19:12 +0200

    usb: xhci: move doorbell array pointer assignment
    Move the doorbell array pointer assignment from xhci_mem_init() to
    xhci_init(), utilizing the new xhci_set_doorbell_ptr() function.
    
    Remove the "// " prefix from trace messages, as it is unnecessary and
    distracting.
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci-caps.h
+++ drivers/usb/host/xhci-caps.h
@@ -63,7 +63,7 @@
 #define CTX_SIZE(_hcc)		(HCC_64BYTE_CONTEXT(_hcc) ? 64 : 32)
 
 /* db_off bitmask - bits 0:1 reserved */
-#define	DBOFF_MASK	(~0x3)
+#define	DBOFF_MASK	(0xfffffffc)
 
 /* run_regs_off bitmask - bits 0:4 reserved */
 #define	RTSOFF_MASK	(~0x1f)
