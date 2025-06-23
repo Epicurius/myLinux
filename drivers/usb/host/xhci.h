16209c86694ac57aa77aec17ebbee2aa90319e89
Niklas Neronin <niklas.neronin@linux.intel.com>
2025-06-23 12:42:32 +0300

    usb: xhci: indent xhci_ep_ctx
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -455,7 +455,7 @@ struct xhci_ep_ctx {
 /* bits 23:16 - Interval period between requests to an endpoint - 125u increments. */
 #define EP_INTERVAL		GENMASK(23, 16)
 /* bits 31:24 - Max Endpoint Service Time Interval Payload High. LEC=1 use bits as ESIT high bits */
-#define EP_MAX_ESIT_PAYLOAD_HI		GENMASK(31, 24)
+#define EP_MAX_ESIT_PAYLOAD_HI	GENMASK(31, 24)
 
 /* ep_info2 bitmasks */
 /* bit 0 - RsvdZ */
@@ -463,33 +463,33 @@ struct xhci_ep_ctx {
 #define EP_ERROR_COUNT		GENMASK(2, 1)
 /* bits 5:3 - Endpoint Type identifies whether an EP CTX is valid. */
 #define EP_TYPE			GENMASK(5, 3)
-#define ISOC_OUT_EP	1
-#define BULK_OUT_EP	2
-#define INT_OUT_EP	3
-#define CTRL_EP		4
-#define ISOC_IN_EP	5
-#define BULK_IN_EP	6
-#define INT_IN_EP	7
+#define ISOC_OUT_EP		1
+#define BULK_OUT_EP		2
+#define INT_OUT_EP		3
+#define CTRL_EP			4
+#define ISOC_IN_EP		5
+#define BULK_IN_EP		6
+#define INT_IN_EP		7
 /* bit 6 - RsvdZ */
 /* bit 7 - Host Initiate Disable, for disabling stream selection */
-#define	EP_HID		BIT(7)
+#define	EP_HID			BIT(7)
 /* bits 15:8 - Max Burst Size, max number of consecutive USB transactions executed. */
-#define EP_MAX_BURST	GENMASK(15, 8)
+#define EP_MAX_BURST		GENMASK(15, 8)
 /* bits 31:16 - Max Packet Size indicates the max packet size that an EP can send and receive. */
 #define MAX_PACKET_MASK		GENMASK(31, 16)
 
 /* tx_info bitmasks */
 /* bits 15:0 - Average TRB Length for EP. */
-#define EP_AVG_TRB_LENGTH		GENMASK(15, 0)
+#define EP_AVG_TRB_LENGTH	GENMASK(15, 0)
 /* bits 31:16 - Max Endpoint Service Time Interval Payload Low. */
-#define EP_MAX_ESIT_PAYLOAD_LO		GENMASK(31, 16)
+#define EP_MAX_ESIT_PAYLOAD_LO	GENMASK(31, 16)
 
 /* deq bitmasks */
 /* bit 0 - Dequeue Cycle State */
-#define EP_CTX_CYCLE_MASK		BIT(0)
+#define EP_CTX_CYCLE_MASK	BIT(0)
 /* bits 3:1 - RsvdZ */
 /* bits 63:4 - TR Dequeue Pointer */
-#define TR_DEQ_PTR_MASK			GENMASK_ULL(63, 4)
+#define TR_DEQ_PTR_MASK		GENMASK_ULL(63, 4)
 
 /*
  * Force Event - generate transfer events for all TRBs for this endpoint
